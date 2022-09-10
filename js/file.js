9c746052ee7f6ea063d45b3493df838c76271aad
ghilanenasreddine <ghilanenasreddine22@gmail.com>
2022-09-10 18:37:52 +0200

    MODIFING THE CODE

--- js/file.js
+++ js/file.js
@@ -1,29 +1,34 @@
 let character = document.querySelector(".character");
 let block = document.querySelector(".block");
-let score = document.querySelector("p .score")
+let score = document.querySelector("p .score");
+let startBtn = k = document.querySelector(".start button")
 let counter = 0;
 
 
-alert("PRESS Ok TO START");
-block.style.animation = "move 1s infinite linear";
+startBtn.onclick = function () {
+    document.querySelector(".start").classList.add("hide");
+    block.style.animation = "move 2s infinite linear";
+    setTimeout(function () {
+        block.style.animation = "move 1s infinite linear";
+    }, 2000)
+}
 
 
-
-document.querySelector("html").addEventListener("click",()=>{
-    if(character.classList!="animate"){
+document.querySelector("html").addEventListener("click", () => {
+    if (character.classList != "animate") {
         character.classList.add("animate");
     }
-    setTimeout(()=>{
+    setTimeout(() => {
         character.classList.remove("animate");
-    },500)
+    }, 500)
 })
 
-let check =setInterval(()=>{
+let check = setInterval(() => {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 
-    if(blockLeft>30 && blockLeft < 60 && characterTop>330){
-        block.style.animation="none"
+    if (blockLeft > 30 && blockLeft < 60 && characterTop > 330) {
+        block.style.animation = "none"
         alert(`YOU LOSE TRY AGAIN YOUR SCORE IS : ${Math.floor(counter / 100)}`);
         counter = 0;
         block.style.animation = "move 1s infinite linear";
@@ -32,10 +37,4 @@ let check =setInterval(()=>{
         counter++;
         score.textContent = counter < 10 ? `0${Math.floor(counter / 100)}` : Math.floor(counter / 100);
     }
-},10);
-
-
-
-
-
-
+}, 10);
